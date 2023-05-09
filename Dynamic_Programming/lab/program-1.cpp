2513007b0003dfb98274c004e976b209710c4ccb
habibor-rahaman1010 <habibor144369@gmail.com>
2023-05-09 22:34:35 +0600

    hello programmer

--- Dynamic_Programming/lab/program-1.cpp
+++ Dynamic_Programming/lab/program-1.cpp
@@ -1,8 +1,31 @@
+//frog 1 problem in c++ solve...
 #include <bits/stdc++.h>
 using namespace std;
 
+const int N = 1e5 + 5;
+int h[N];
+int dp[N];
+
+int ston(int n){
+    if(n == 1) return 0;
+    if(dp[n] != -1) return dp[n];
+
+    int ans1 = ston(n - 1) + abs(h[n] - h[n - 1]);
+    int ans2 = ston(n - 2) + abs(h[n] - h[n - 2]);
+    int ans = min(ans1, ans2);
+    dp[n] = ans;
+    return ans;
+}
+
 int main(){
-    cout<<"Hello programmer";
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        cin >> h[i];
+    }  
+    memset(dp, -1, sizeof(dp));
+    int ans =  ston(n);
+    cout << ans;
 
 return 0;
 }
