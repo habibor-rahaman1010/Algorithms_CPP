0f64a1c5c27702338265e8a9233785ba66a52ad0
habibor-rahaman1010 <habibor144369@gmail.com>
2023-05-13 22:42:25 +0600

    ok

---
+++ problem_Solve2/problem-4.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+  int n, sum;
+  cin >> n >> sum;
+  
+  vector<int> nums(n + 1);
+  for (int i = 0; i < n; i++) {
+    cin >> nums[i];
+  }
+  unordered_map<int, int> ht;
+  for (int i = 0; i < n; i++) {
+    auto it = ht.find(sum - nums[i]);
+    if(it != ht.end()) {
+      cout << it->second + 1 << " " << i + 1 << "\n";
+      return 0;
+    }
+    ht[nums[i]] = i;
+  }
+
+  cout << "IMPOSSIBLE" << "\n";
+  
+  return 0;
+}
